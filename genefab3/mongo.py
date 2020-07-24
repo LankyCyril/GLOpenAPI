891ab6078bfdf4f8ce3057edfd911ecf25323708
LankyCyril <cyril@ro.ru>
2020-07-24 13:50:26 -0400

    Proper caching of jsons in mongo

--- genefab3/mongo.py
+++ genefab3/mongo.py
@@ -3,6 +3,7 @@ from genefab3.config import COLD_API_ROOT, MAX_JSON_AGE
 from datetime import datetime
 from genefab3.json import download_cold_json
 from genefab3.exceptions import GeneLabJSONException
+from pymongo import DESCENDING
 
 
 def is_json_cache_fresh(json_cache_info, max_age=MAX_JSON_AGE):
@@ -15,21 +16,30 @@ def is_json_cache_fresh(json_cache_info, max_age=MAX_JSON_AGE):
 
 
 def get_fresh_json(db, identifier, kind="other", max_age=MAX_JSON_AGE):
-    json_cache_info = db.json_cache.find_one({
-        "identifier": identifier, "kind": kind,
-        # TODO: grab latest in case stray ones found
-    })
+    json_cache_info = db.json_cache.find_one(
+        {"identifier": identifier, "kind": kind},
+        sort=[("timestamp", DESCENDING)],
+    )
     if is_json_cache_fresh(json_cache_info, max_age):
         return json_cache_info["raw"]
     else:
-        # TODO: remove stale json here
-        json = download_cold_json(identifier, kind=kind)
-        db.json_cache.insert_one({
-            "identifier": identifier, "kind": kind,
-            "timestamp": int(datetime.now().timestamp()),
-            "raw": json,
-        })
-        return json
+        try:
+            json = download_cold_json(identifier, kind=kind)
+        except Exception:
+            try:
+                return json_cache_info["raw"]
+            except (TypeError, KeyError):
+                raise GeneLabJSONException("Cannot retrieve cold storage JSON")
+        else:
+            db.json_cache.delete_many(
+                {"identifier": identifier, "kind": kind},
+            )
+            db.json_cache.insert_one({
+                "identifier": identifier, "kind": kind,
+                "timestamp": int(datetime.now().timestamp()),
+                "raw": json,
+            })
+            return json
 
 
 def refresh_json_store_inner(db):
@@ -42,6 +52,7 @@ def refresh_json_store_inner(db):
 
 
 def refresh_json_store(db):
+    """Keep all dataset and assay metadata up to date"""
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
