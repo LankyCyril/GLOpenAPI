2500b8599432399c068c73af9b7e24828a582c60
LankyCyril <cyril@ro.ru>
2020-08-05 14:18:01 -0400

    Refactoring + preface in interactive mode

--- genefab3/display.py
+++ genefab3/display.py
@@ -31,18 +31,18 @@ DF_STATIC_CSS_SHADING = "    td.col{} {{background: #E3E3E399 !important;}}"
 with open(join(split(split(abspath(__file__))[0])[0], "html/df.html")) as html:
     DF_DYNAMIC_HTML = html.read()
 
-DF_DYNAMIC_FORMATTER_GLDS_MASK = """columns[0].formatter=function(r,c,v,d,x){{
-    return "<a href='{}&select="+v+"'>"+v+"</a>";
-}};
-columns[0].momentarilyFormattable = false;"""
 
-DF_DYNAMIC_FORMATTER_ASSAY_MASK = """columns[1].formatter=function(r,c,v,d,x){{
-    return "<a href='{}&select="+data[r]["{}"]+":"+v+"'>"+v+"</a>";
-}};
-columns[1].momentarilyFormattable = false;"""
+def annotated_cols(dataframe, sep):
+    """Format (multi)-columns of dataframe with '#' prepended"""
+    return sub(
+        r'^', "#", sub(
+            r'\n(.)', r'\n#\1',
+            dataframe.columns.to_frame().T.to_csv(sep=sep, **DF_KWS),
+        ),
+    )
 
 
-def get_dataframe_css(columns):
+def get_static_dataframe_css(columns):
     """Add shading of 'info' columns to default DF_STATIC_CSS"""
     shading_css_lines = []
     for i, col in enumerate(columns.get_level_values(0)):
@@ -59,17 +59,7 @@ def get_dataframe_css(columns):
         return DF_STATIC_CSS
 
 
-def annotated_cols(dataframe, sep):
-    """Format (multi)-columns of dataframe with '#' prepended"""
-    return sub(
-        r'^', "#", sub(
-            r'\n(.)', r'\n#\1',
-            dataframe.columns.to_frame().T.to_csv(sep=sep, **DF_KWS),
-        ),
-    )
-
-
-def color_bool(x):
+def static_color_bool(x):
     """Highlight True in green and False in pale red, NA in lightgray italics, keep everything else as-is"""
     if x == True:
         return "color: green"
@@ -81,8 +71,45 @@ def color_bool(x):
         return ""
 
 
-def get_dynamic_dataframe_html(df, cur_url):
-    """Display dataframe using SlickGrid"""
+def build_url(request, replace={}, drop=set()):
+    """Rebuild URL from request, alter based on `replace` and `drop`"""
+    base_url = request.base_url
+    for old, new in replace.items():
+        base_url = base_url.replace(old, new)
+    url_components = [base_url + "?"]
+    for arg in request.args:
+        if arg not in drop:
+            for value in request.args.getlist(arg):
+                if value == "":
+                    url_components.append(arg+"&")
+                else:
+                    url_components.append("{}={}&".format(arg, value))
+    return "".join(url_components)
+
+
+def get_dynamic_glds_formatter(request):
+    """Get SlickGrid formatter for column 'accession'"""
+    mask = """columns[0].formatter=function(r,c,v,d,x){{
+        return "<a href='{}select="+v+"'>"+v+"</a>";
+    }};
+    columns[0].momentarilyFormattable = false;"""
+    return mask.format(build_url(request, drop={"select"}))
+
+
+def get_dynamic_assay_formatter(request, shortnames):
+    """Get SlickGrid formatter for column 'assay name'"""
+    mask = """columns[1].formatter=function(r,c,v,d,x){{
+        return "<a href='{}select="+data[r]["{}"]+":"+v+"'>"+v+"</a>";
+    }};
+    columns[1].momentarilyFormattable = false;"""
+    return mask.format(
+        build_url(request, replace={"/assays/": "/samples/"}, drop={"select"}),
+        shortnames[0],
+    )
+
+
+def get_dynamic_twolevel_dataframe_html(df, request):
+    """Display dataframe with two-level columns using SlickGrid"""
     shortnames = []
     def generate_short_names(*args):
         s, j = "", len(shortnames) + 1
@@ -90,42 +117,44 @@ def get_dynamic_dataframe_html(df, cur_url):
             s, j = chr(((j % 26) or 26) + 96) + s, (j - 1) // 26
         shortnames.append(s)
         return s
-    if df.columns.nlevels == 2:
-        rowdata = (
-            df.droplevel(0, axis=1).rename(generate_short_names, axis=1)
-            .applymap(lambda x: "NA" if isnull(x) else str(x))
-            .to_json(orient="records")
-        )
-        columndata = "\n".join((
-            "{{id:'{}',field:'{}',columnGroup:'{}',name:'{}'}},".format(
-                sn, sn, level0, level1,
-            )
-            for (level0, level1), sn in zip(df.columns, shortnames)
-        ))
-        glv1 = df.columns.get_level_values(1)
-        if (glv1[:2] == ["accession", "assay name"]).all():
-            formatters = "\n".join([
-                DF_DYNAMIC_FORMATTER_GLDS_MASK.format(cur_url),
-                DF_DYNAMIC_FORMATTER_ASSAY_MASK.format(
-                    cur_url.replace("/assays/", "/samples/"),
-                    shortnames[0]
-                ),
-            ])
-        else:
-            formatters = ""
+    rowdata = (
+        df.droplevel(0, axis=1).rename(generate_short_names, axis=1)
+        .applymap(lambda x: "NA" if isnull(x) else str(x))
+        .to_json(orient="records")
+    )
+    cdm = "{{id:'{}',field:'{}',columnGroup:'{}',name:'{}'}},"
+    columndata = "\n".join(
+        cdm.format(n, n, a, b) for (a, b), n in zip(df.columns, shortnames)
+    )
+    if all(df.columns.get_level_values(1)[:2] == ["accession", "assay name"]):
+        formatters = "\n".join([
+            get_dynamic_glds_formatter(request),
+            get_dynamic_assay_formatter(request, shortnames),
+        ])
     else:
-        raise NotImplementedError("Laterz")
-    return sub(
-        r'\/\/ ROWDATA', rowdata, sub(
-            r'\/\/ COLUMNDATA', columndata, sub(
-                r'\/\/ FORMATTERS', formatters, DF_DYNAMIC_HTML,
-            )
-        )
+        formatters = ""
+    htmlink = build_url(request, drop={"fmt"}) + "fmt=html"
+    csvlink = build_url(request, drop={"fmt"}) + "fmt=csv"
+    tsvlink = build_url(request, drop={"fmt"}) + "fmt=tsv"
+    return (
+        DF_DYNAMIC_HTML
+        .replace("// FORMATTERS", formatters).replace("HTMLINK", htmlink)
+        .replace("CSVLINK", csvlink).replace("TSVLINK", tsvlink)
+        .replace("// COLUMNDATA", columndata).replace("// ROWDATA", rowdata)
     )
 
 
-def display_dataframe(df, fmt, cur_url):
+def get_dynamic_dataframe_html(df, request):
+    """Display dataframe using SlickGrid"""
+    if df.columns.nlevels == 2:
+        return get_dynamic_twolevel_dataframe_html(df, request)
+    else:
+        raise NotImplementedError("Non-two-level interactive dataframe")
+
+
+def display_dataframe(df, request):
     """Display dataframe with specified format"""
+    fmt = request.args.get("fmt", "tsv")
     if fmt == "tsv":
         content = annotated_cols(df, sep="\t") + df.to_csv(sep="\t", **DF_KWS)
         mimetype = "text/plain"
@@ -133,13 +162,13 @@ def display_dataframe(df, fmt, cur_url):
         content = annotated_cols(df, sep=",") + df.to_csv(sep=",", **DF_KWS)
         mimetype = "text/plain"
     elif fmt == "html":
-        content = get_dataframe_css(df.columns) + (
-            df.style.format(None, na_rep="NA").applymap(color_bool)
+        content = get_static_dataframe_css(df.columns) + (
+            df.style.format(None, na_rep="NA").applymap(static_color_bool)
             .hide_index().render()
         )
         mimetype = "text/html"
     elif fmt == "interactive":
-        content = get_dynamic_dataframe_html(df, cur_url)
+        content = get_dynamic_dataframe_html(df, request)
         mimetype = "text/html"
     else:
         raise NotImplementedError("fmt='{}'".format(fmt))
@@ -149,9 +178,7 @@ def display_dataframe(df, fmt, cur_url):
 def display(obj, request):
     """Dispatch object and trailing request arguments to display handler"""
     if isinstance(obj, DataFrame):
-        return display_dataframe(
-            obj, request.args.get("fmt", "tsv"), request.url,
-        )
+        return display_dataframe(obj, request)
     else:
         raise NotImplementedError(
             "Display of {}".format(str(type(obj).strip("<>")))
