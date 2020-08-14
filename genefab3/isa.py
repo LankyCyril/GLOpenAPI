9bef1b132b892a5bdabe49b1a08a446e25feb0d5
LankyCyril <cyril@ro.ru>
2020-08-14 02:38:11 -0400

    Refactoring

--- genefab3/isa.py
+++ genefab3/isa.py
@@ -35,11 +35,13 @@ def isatomic(variable):
         return True
 
 
-def populate(_what=Namespace(), _using={}, _via=(None,), _lengths=(Any,), _toplevel_method=None, _each=None, _copy_atoms=True, _copy_atomic_lists=True, _raised=(), **kwargs):
+def populate(_what=Namespace(), _using={}, _via=None, _lengths=Any, _toplevel_method=None, _each=None, _copy_atoms=True, _copy_atomic_lists=True, _raised=(), **kwargs):
     if not isinstance(_via, (list, tuple)):
         _via = [_via]
     if not isinstance(_lengths, (list, tuple)):
         _lengths = [_lengths]
+    if not isinstance(_raised, (list, tuple)):
+        _raised = [_raised]
     if (not _using) or (len(_via) == 0):
         raise GeneLabJSONException("Reached a dead end in JSON")
     source = descend(_using, _via, _lengths)
@@ -71,11 +73,10 @@ def populate(_what=Namespace(), _using={}, _via=(None,), _lengths=(Any,), _tople
         return source
 
 
-def Parser(_via=(None,), _lengths=(Any,), _toplevel_method=None, _each=None, **kwargs):
+def Parser(_via=None, _lengths=Any, _toplevel_method=None, _each=None, **kwargs):
     return partial(
-        populate, _via=_via, _lengths=_lengths,
-        _toplevel_method=_toplevel_method, _each=_each,
-        **kwargs,
+        populate, _via=_via, _lengths=_lengths, _each=_each,
+        _toplevel_method=_toplevel_method, **kwargs,
     )
 
 
@@ -83,27 +84,23 @@ def sparse_json_to_dataframe(entries):
     return repr(entries)[:50] + "..."
 
 
-def sparse_json_to_many_dataframes(_using, ignore={AttributeError}):
+def sparse_json_to_many_dataframes(_using, ignore=(AttributeError,)):
     try:
-        return {
-            key: sparse_json_to_dataframe(entries)
-            for key, entries in _using.items()
-        }
-    except Exception as e:
-        if type(e) in ignore:
-            pass
+        return {k: sparse_json_to_dataframe(e) for k, e in _using.items()}
+    except ignore:
+        pass
 
 
 class ISA(Namespace):
     def __init__(self, json):
         parser = Parser([None, 0], [1, Any],
             doi=Parser(["doiFields", 0, "doi"], [1, Any, Atom]),
-            _raised=[Parser(["foreignFields", 0, "isa2json"], [1, Any, Any],
+            _raised=Parser(["foreignFields", 0, "isa2json"], [1, Any, Any],
                 _copy_atoms=False,
-                _raised=[Parser("additionalInformation", Any,
-                    _raised=[Parser(_each=sparse_json_to_many_dataframes)],
+                _raised=Parser("additionalInformation", Any,
+                    _raised=Parser(_each=sparse_json_to_many_dataframes),
                     assays2=Parser("assays", 1, sparse_json_to_many_dataframes),
-                )],
-            )],
+                ),
+            ),
         )
         parser(_what=self, _using=json)
