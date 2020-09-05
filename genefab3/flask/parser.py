85d72407af9e36b69278d282e465d2df225afd3e
LankyCyril <cyril@ro.ru>
2020-09-04 23:48:01 -0400

    Parse 'select='

--- genefab3/flask/parser.py
+++ genefab3/flask/parser.py
@@ -1,44 +1,56 @@
-from re import sub, escape, search
+from re import sub, escape
 from argparse import Namespace
 from genefab3.config import ANNOTATION_CATEGORIES
 from genefab3.utils import UniversalSet
 
 
+def select_pair_to_query(key, value):
+    """Interpret single key-value pair for dataset / assay constraint"""
+    if value.count(".") == 0:
+        yield {".accession": value}, None
+    elif value.count(".") == 1:
+        accession, assay_name = value.split(".")
+        yield {".accession": accession, ".assay": assay_name}, None
+
+
 def pair_to_query(isa_category, fields, value, constrain_to=UniversalSet(), dot_postfix=False):
     """Interpret single key-value pair if it gives rise to database query"""
     if fields[0] in constrain_to:
-        for component in (isa_category, *fields, value):
-            if search(r'\$', component):
-                break
+        if (len(fields) == 2) and (dot_postfix == "auto"):
+            lookup_key = ".".join([isa_category] + fields) + "."
+            projection = {lookup_key + ".": True}
         else:
-            if (len(fields) == 2) and (dot_postfix == "auto"):
-                lookup_key = ".".join([isa_category] + fields) + "."
-                projection = {lookup_key + ".": True}
-            else:
-                lookup_key = ".".join([isa_category] + fields)
-                projection = {lookup_key: True}
-            if value:
-                query = {lookup_key: {"$in": value.split("|")}}
-            else:
-                query = {lookup_key: {"$exists": True}}
-            yield query, projection
+            lookup_key = ".".join([isa_category] + fields)
+            projection = {lookup_key: True}
+        if value:
+            query = {lookup_key: {"$in": value.split("|")}}
+        else:
+            query = {lookup_key: {"$exists": True}}
+        yield query, projection
 
 
 def request_pairs_to_queries(rargs, key):
     """Interpret key-value pairs if they give rise to database queries"""
-    isa_category, *fields = key.split(".")
-    if fields:
+    if key == "select":
         for value in rargs.getlist(key):
-            if isa_category == "investigation":
-                yield from pair_to_query(
-                    isa_category, fields, value,
-                    constrain_to=UniversalSet(), dot_postfix=False,
-                )
-            elif isa_category in {"study", "assay"}:
-                yield from pair_to_query(
-                    isa_category, fields, value,
-                    constrain_to=ANNOTATION_CATEGORIES, dot_postfix="auto",
-                )
+            if "$" not in value:
+                yield from select_pair_to_query(key, value)
+    elif "$" not in key:
+        isa_category, *fields = key.split(".")
+        if fields:
+            for value in rargs.getlist(key):
+                if "$" not in value:
+                    if isa_category == "investigation":
+                        yield from pair_to_query(
+                            isa_category, fields, value,
+                            constrain_to=UniversalSet(), dot_postfix=False,
+                        )
+                    elif isa_category in {"study", "assay"}:
+                        yield from pair_to_query(
+                            isa_category, fields, value,
+                            constrain_to=ANNOTATION_CATEGORIES,
+                            dot_postfix="auto",
+                        )
 
 
 def parse_request(request):
