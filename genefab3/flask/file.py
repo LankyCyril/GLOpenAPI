0fe09b668a3a8af7cc92438b11c8b9545ed8270a
LankyCyril <cyril@ro.ru>
2020-11-21 21:52:33 -0500

    Match files via GLDS.resolve_filename()

--- genefab3/flask/file.py
+++ genefab3/flask/file.py
@@ -1,5 +1,6 @@
 from genefab3.exceptions import GeneLabFileException
 from genefab3.mongo.dataset import CachedDataset
+from re import escape
 from urllib.request import urlopen
 
 
@@ -8,9 +9,15 @@ def get_file(db, context):
     glds = CachedDataset(
         db=db, accession=context.accessions[0], init_assays=False,
     )
-    filename = context.kwargs["filename"]
-    if filename not in glds.fileurls:
-        raise GeneLabFileException("File not found")
+    mask = context.kwargs["filename"]
+    if (mask[0] == "/") and (mask[-1] == "/"): # regular expression passed
+        fileinfo = glds.resolve_filename(mask[1:-1])
+    else: # simple filename passed, match full
+        fileinfo = glds.resolve_filename(r'^'+escape(mask)+r'$')
+    if not fileinfo:
+        raise GeneLabFileException("Requested file not found")
+    elif len(fileinfo) > 1:
+        raise GeneLabFileException("Multiple files match search criteria")
     else:
-        with urlopen(glds.fileurls[filename]) as response:
+        with urlopen(next(iter(fileinfo.values())).url) as response:
             return response.read()
