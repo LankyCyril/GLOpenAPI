65801fbf8e8988e2c8d030eb8756b46289850aca
LankyCyril <cyril@ro.ru>
2020-12-12 21:50:06 -0500

    Cache response with timestamp, api_args, retrieve with api_args

--- genefab3/backend/sql/readers/cache.py
+++ genefab3/backend/sql/readers/cache.py
@@ -1,21 +1,26 @@
 from genefab3.config import RESPONSE_CACHE
+from urllib.request import quote
+from json import dumps
 from contextlib import closing
 from sqlite3 import connect, OperationalError
-from urllib.request import quote
 from flask import Response
 
 
 def retrieve_cached_response(context, response_cache=RESPONSE_CACHE, table="response_cache"):
     """Retrieve cached response object blob from response_cache table if possible; otherwise return None"""
-    api_path = quote(context.full_path)
-    query = f"""SELECT * FROM '{table}' WHERE api_path = '{api_path}'"""
+    api_args = quote(dumps(context.complete_args, sort_keys=True))
+    query = f"""
+        SELECT response, mimetype FROM '{table}'
+        WHERE api_args = '{api_args}'
+        LIMIT 1
+    """
     try:
         with closing(connect(response_cache)) as sql_connection:
             cursor = sql_connection.cursor()
             row = cursor.execute(query).fetchone()
     except OperationalError:
         row = None
-    if isinstance(row, tuple) and (len(row) >= 3):
-        return Response(response=row[1], mimetype=row[2])
+    if isinstance(row, tuple) and (len(row) == 2):
+        return Response(response=row[0], mimetype=row[1])
     else:
         return None
--- genefab3/backend/sql/writers/cache.py
+++ genefab3/backend/sql/writers/cache.py
@@ -1,11 +1,16 @@
 from os import path, makedirs
 from genefab3.config import RESPONSE_CACHE
 from urllib.request import quote
+from json import dumps
+from datetime import datetime
+from sqlite3 import Binary, connect, OperationalError
 from contextlib import closing
-from sqlite3 import connect, Binary, OperationalError
 
 
-RESPONSE_CACHE_SCHEMA = "('api_path' TEXT, 'response' BLOB, 'mimetype' TEXT)"
+RESPONSE_CACHE_SCHEMA = """(
+    'api_args' TEXT, 'api_path' TEXT, 'timestamp' INTEGER,
+    'response' BLOB, 'nbytes' INTEGER, 'mimetype' TEXT
+)"""
 
 
 def ensure_response_lru_cache(response_cache=RESPONSE_CACHE, table="response_cache", schema=RESPONSE_CACHE_SCHEMA):
@@ -13,26 +18,38 @@ def ensure_response_lru_cache(response_cache=RESPONSE_CACHE, table="response_cac
     if not path.exists(path.dirname(response_cache)):
         makedirs(path.dirname(response_cache))
         with closing(connect(response_cache)) as sql_connection:
-            cursor = sql_connection.cursor()
-            cursor.execute("PRAGMA auto_vacuum = 1")
-            cursor.execute(f"CREATE TABLE IF NOT EXISTS '{table}' {schema}")
+            sql_connection.cursor().execute("PRAGMA auto_vacuum = 1")
+    with closing(connect(response_cache)) as sql_connection:
+        sql_connection.cursor().execute(
+            f"CREATE TABLE IF NOT EXISTS '{table}' {schema}",
+        )
+        sql_connection.commit()
 
 
 def cache_response(context, response, response_cache=RESPONSE_CACHE, table="response_cache", schema=RESPONSE_CACHE_SCHEMA):
     """Store response object blob in response_cache table, if possible"""
     ensure_response_lru_cache(response_cache, table, schema)
     api_path = quote(context.full_path)
+    api_args = quote(dumps(context.complete_args, sort_keys=True))
+    blob = Binary(response.get_data())
+    timestamp = int(datetime.now().timestamp())
     try:
         with closing(connect(response_cache)) as sql_connection:
             cursor = sql_connection.cursor()
             cursor.execute(
-                f"DELETE FROM '{table}' WHERE api_path = '{api_path}'",
+                f"DELETE FROM '{table}' WHERE api_args = '{api_args}'",
             )
-            query = f"""
-                INSERT INTO '{table}' (api_path, response, mimetype)
-                VALUES ('{api_path}', ?, '{response.mimetype}')
+            action = f"""
+                INSERT INTO '{table}' (
+                    api_args, api_path, timestamp,
+                    response, nbytes, mimetype
+                )
+                VALUES (
+                    '{api_args}', '{api_path}', {timestamp},
+                    ?, {blob.nbytes}, '{response.mimetype}'
+                )
             """
-            cursor.execute(query, [Binary(response.get_data())])
+            cursor.execute(action, [blob])
             sql_connection.commit()
     except OperationalError:
         pass
