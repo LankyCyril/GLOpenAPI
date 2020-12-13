4d4a931488131cc91c54dfb767481c69ee2e5e51
LankyCyril <cyril@ro.ru>
2020-12-12 22:52:06 -0500

    Ensure LRU cache more gracefully

--- genefab3/backend/sql/writers/cache.py
+++ genefab3/backend/sql/writers/cache.py
@@ -20,8 +20,10 @@ RESPONSE_CACHE_SCHEMA = """(
 
 def ensure_response_lru_cache(response_cache=RESPONSE_CACHE, table="response_cache", schema=RESPONSE_CACHE_SCHEMA):
     """Ensure parent directory exists; will fail with generic Python exceptions here or downstream if not a writable dir"""
-    if not path.exists(path.dirname(response_cache)):
-        makedirs(path.dirname(response_cache))
+    parent_path = path.dirname(response_cache)
+    if (parent_path not in {"", "."}) and (not path.exists(parent_path)):
+        makedirs(parent_path)
+    if not path.exists(response_cache):
         with closing(connect(response_cache)) as sql_connection:
             sql_connection.cursor().execute("PRAGMA auto_vacuum = 1")
     with closing(connect(response_cache)) as sql_connection:
