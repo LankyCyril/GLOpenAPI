eb73ed81e42e59674649c532bb679ed15eaafc1d
LankyCyril <cyril@ro.ru>
2021-03-17 15:36:58 -0400

    Dev: temporarily include tests.py

---
+++ tests.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from pymongo import MongoClient
+from os import path
+from pickle import load, dump
+from json import dumps
+from genefab3.db.mongo.document import CachedDocumentByValue
+from genefab3_genelab_adapter.methods import get_dataset_files
+from genefab3.isa.types import Dataset
+
+
+def dmp(j):
+    print(dumps(j, indent=4, sort_keys=True))
+
+
+mongo_client = MongoClient()
+db = mongo_client.test_db
+
+
+PKL = "sandbox/GLDS-48-files.pkl"
+if path.isfile(PKL):
+    with open(PKL, mode="rb") as pkl:
+        files = load(pkl)
+else:
+    files = CachedDocumentByValue(
+        identifier=dict(kind="dataset files", accession="GLDS-48"),
+        collection=db.records,
+        value=get_dataset_files("GLDS-48"),
+    )
+    with open(PKL, mode="wb") as pkl:
+        dump(files, pkl)
+
+glds = Dataset("GLDS-48", files.value, None, "sandbox/testblobs.db")
+#print(files.base64value)
+print(files.changed, glds.isa.changed)
+sample = next(glds.samples)
+
+from bson import BSON
+from sys import getsizeof
+print(getsizeof(BSON.encode(sample)))
+
+from copy import deepcopy
+db.tests.insert_one(deepcopy(sample))
+
+dmp(db.tests.find_one(
+    {"Files.datatype": "pca"},
+    {"Files": {"$elemMatch": {"datatype": "pca"}}, "_id": False},
+))
+
+from genefab3.db.mongo.utils import harmonize_document
+dmp(harmonize_document(sample, units_format="{value} {{{unit}}}"))
