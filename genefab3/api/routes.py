18ff9332b428e9b92dc00f91bb7d95c96b9ea370
LankyCyril <cyril@ro.ru>
2021-03-20 12:45:02 -0400

    Debug endpoints

--- genefab3/api/routes.py
+++ genefab3/api/routes.py
@@ -1,5 +1,6 @@
 from functools import wraps, partial
 from genefab3.common.logger import GeneFabLogger
+from genefab3.common.exceptions import GeneFabException
 
 
 class Routes():
@@ -11,7 +12,7 @@ class Routes():
         if endpoint:
             wrapper.endpoint = endpoint
         elif hasattr(method, "__name__") and isinstance(method.__name__, str):
-            wrapper.endpoint = "/" + method.__name__ + "/"
+            wrapper.endpoint = "/" + method.__name__.replace("_", "/") + "/"
         return wrapper
  
     def items(self):
@@ -20,11 +21,24 @@ class Routes():
             if hasattr(method, "endpoint"):
                 yield method.endpoint, method
  
+    @partial(_as_endpoint, endpoint="/favicon.<imgtype>")
+    def favicon(self, imgtype):
+        return ""
+ 
+    @_as_endpoint
+    def debug_warning(self):
+        GeneFabLogger().warning("Generic warning test")
+        return "OK (emitted warning)"
+ 
+    @_as_endpoint
+    def debug_error(self):
+        raise GeneFabException("Generic error test")
+        return "OK (raised exception)"
+ 
     @partial(_as_endpoint, endpoint="/")
     def root(self):
         return "Hello space"
  
     @_as_endpoint
     def status(self):
-        GeneFabLogger().warning("/status/ not implemented")
-        return "None"
+        return "OK"
