02c9ae9e73d82c946200a930b20fc13ceaf94c27
LankyCyril <cyril@ro.ru>
2021-03-22 17:30:22 -0400

    Write status

--- genefab3/db/mongo/cacher.py
+++ genefab3/db/mongo/cacher.py
@@ -1,10 +1,13 @@
 from threading import Thread
 from genefab3.common.logger import GeneFabLogger
+from types import SimpleNamespace
 from time import sleep
 from collections import OrderedDict
 from genefab3.db.mongo.types import CachedDocumentByValue
 from genefab3.isa.types import Dataset
 from genefab3.db.mongo.utils import run_mongo_transaction, harmonize_document
+from functools import partial
+from genefab3.db.mongo.status import update_status
 
 
 class CacherThread(Thread):
@@ -18,6 +21,11 @@ class CacherThread(Thread):
         self.metadata_retry_delay = metadata_retry_delay
         self.units_formatter = units_formatter
         self.logger = GeneFabLogger()
+        self.collections = SimpleNamespace( # TODO pass CNAMEs from Client
+            metadata=self.mongo_db.metadata,
+            records=self.mongo_db.records,
+            status=self.mongo_db.status,
+        )
         super().__init__()
  
     def run(self):
@@ -38,6 +46,9 @@ class CacherThread(Thread):
     def recache_metadata(self):
         """Instantiate each available dataset; if contents changed, dataset automatically updates db.metadata"""
         self.logger.info("CacherThread: Checking metadata cache")
+        _update_status = partial(
+            update_status, collection=self.collections.status,
+        )
         try:
             accessions = OrderedDict(
                 cached=set(self.mongo_db.metadata.distinct("info.accession")),
@@ -48,15 +59,19 @@ class CacherThread(Thread):
             self.logger.error(f"CacherThread: {repr(e)}")
             return None, False
         for accession in accessions["cached"] - accessions["live"]:
-            key, report = self.drop_single_dataset_metadata(accession)
+            key, report, error = self.drop_single_dataset_metadata(accession)
             accessions[key].add(accession)
             self.logger.info(f"CacherThread/{accession}: {report}")
+            _update_status(accession=accession, status=key)
         for accession in accessions["live"]:
-            key, report = self.recache_single_dataset_metadata(accession)
+            key, report, error = self.recache_single_dataset_metadata(accession)
             accessions[key].add(accession)
-            _func = self.logger.error if key == "failed" else self.logger.info
-            _func(f"CacherThread/{accession}: {report}")
-            # TODO: status, see genefab3/legacy/backend/mongo/dataset.py
+            if key == "failed":
+                self.logger.error(f"CacherThread/{accession}: {report}")
+                _update_status(accession=accession, status=key, error=error)
+            else:
+                self.logger.info(f"CacherThread/{accession}: {report}")
+                _update_status(accession=accession, status=key)
         self.logger.info(
             "CacherThread, datasets: " + ", ".join(
                 f"{k}={len(v)}" for k, v in accessions.items()
@@ -67,29 +82,42 @@ class CacherThread(Thread):
     def drop_single_dataset_metadata(self, accession):
         """Drop all metadata entries associated with `accession` from `self.mongo_db.metadata`"""
         run_mongo_transaction(
-            "delete_many", self.mongo_db.metadata, # TODO pass CNAME from Client
+            "delete_many", self.collections.metadata,
             query={"info.accession": accession},
         )
-        return "dropped", "removed from database"
+        return "dropped", "removed from database", None
  
     def recache_single_dataset_metadata(self, accession):
         """Check if dataset changed, update metadata cached in `self.mongo_db.metadata`, report with result/errors"""
+        _update_status = partial(
+            update_status, collection=self.collections.status,
+            accession=accession, status="warning",
+        )
         files = CachedDocumentByValue(
             identifier=dict(kind="dataset files", accession=accession),
-            collection=self.mongo_db.records, # TODO pass CNAME from Client
+            collection=self.collections.records,
             value=self.adapter.get_files_by_accession(accession),
         )
         if files.changed:
             try:
                 dataset = Dataset(accession, files.value, self.sqlite_dbs.blobs)
             except Exception as e:
-                return "failed", f"failed to update ({repr(e)}), kept stale"
+                return "failed", f"failed to update ({repr(e)}), kept stale", e
             else:
                 self.drop_single_dataset_metadata(accession)
+                has_samples = False
                 for sample in dataset.samples:
                     self.mongo_db.metadata.insert_one(
                         harmonize_document(sample, self.units_formatter),
                     )
-                return "updated", "updated"
+                    has_samples = True
+                    if "Study" not in sample:
+                        _update_status(
+                            sample_name=sample.name,
+                            warning="Study entry missing",
+                        )
+                if not has_samples:
+                    _update_status(warning="No samples")
+                return "updated", "updated", None
         else:
-            return "fresh", "no action (fresh)"
+            return "fresh", "no action (fresh)", None
---
+++ genefab3/db/mongo/status.py
@@ -0,0 +1,30 @@
+from genefab3.db.mongo.utils import run_mongo_transaction
+from datetime import datetime
+
+
+def update_status(collection, accession, sample_name=None, status="success", warning=None, error=None, details=()):
+    """Update status of dataset (and, optionally, assay/sample) in `collection`"""
+    if sample_name is None:
+        replacement_query = {"kind": "dataset", "accession": accession}
+        if status == "failure":
+            run_mongo_transaction(
+                action="delete_many", collection=collection,
+                query={"accession": accession},
+            )
+    else:
+        replacement_query = {
+            "kind": "sample",
+            "accession": accession, "sample name": sample_name,
+        }
+    inserted_data = {
+        "status": status, "warning": warning,
+        "error": None if (error is None) else type(error).__name__,
+        "details": list(details),
+        "report timestamp": int(datetime.now().timestamp())
+    }
+    if error is not None:
+        inserted_data["details"].extend(getattr(error, "args", []))
+    run_mongo_transaction(
+        action="replace", collection=collection,
+        query=replacement_query, data=inserted_data,
+    )
