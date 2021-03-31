0b5a9bc5b47b15982abb48b98df58695afaf270a
LankyCyril <cyril@ro.ru>
2021-03-31 13:03:11 -0400

    More straightforward implementation of decorator as_endpoint

--- genefab3/api/routes.py
+++ genefab3/api/routes.py
@@ -1,4 +1,4 @@
-from functools import wraps, partial
+from functools import wraps
 from genefab3.common.exceptions import GeneFabException
 from genefab3.api import views
 
@@ -8,16 +8,19 @@ class Routes():
     def __init__(self, mongo_collections):
         self.mongo_collections = mongo_collections
  
-    def _as_endpoint(method, endpoint=None, fmt="tsv"):
-        @wraps(method)
-        def wrapper(*args, **kwargs):
-            return method(*args, **kwargs)
-        if endpoint:
-            wrapper.endpoint = endpoint
-        elif hasattr(method, "__name__") and isinstance(method.__name__, str):
-            wrapper.endpoint = "/" + method.__name__ + "/"
-        wrapper.fmt = fmt
-        return wrapper
+    def as_endpoint(*, endpoint=None, fmt="tsv"):
+        def outer(method):
+            @wraps(method)
+            def inner(*args, **kwargs):
+                return method(*args, **kwargs)
+            if endpoint:
+                inner.endpoint = endpoint
+            elif hasattr(method, "__name__"):
+                if isinstance(method.__name__, str):
+                    inner.endpoint = "/" + method.__name__ + "/"
+            inner.fmt = fmt
+            return inner
+        return outer
  
     def items(self):
         for name in dir(self):
@@ -25,23 +28,23 @@ class Routes():
             if isinstance(getattr(method, "endpoint", None), str):
                 yield method.endpoint, method
  
-    @partial(_as_endpoint, endpoint="/favicon.<imgtype>", fmt="raw")
+    @as_endpoint(endpoint="/favicon.<imgtype>", fmt="raw")
     def favicon(self, imgtype):
         return ""
  
-    @partial(_as_endpoint, endpoint="/debug/", fmt="raw")
+    @as_endpoint(endpoint="/debug/", fmt="raw")
     def debug(self):
         return "OK"
  
-    @partial(_as_endpoint, endpoint="/debug/error/", fmt="raw")
+    @as_endpoint(endpoint="/debug/error/", fmt="raw")
     def debug_error(self):
         raise GeneFabException("Generic error test")
         return "OK (raised exception)"
  
-    @partial(_as_endpoint, endpoint="/", fmt="html")
+    @as_endpoint(endpoint="/", fmt="html")
     def root(self):
         return "Hello space"
  
-    @_as_endpoint
+    @as_endpoint()
     def status(self):
         return views.status.get(self.mongo_collections)
