dda848229d196927157a6751a7d50d39a399978a
LankyCyril <cyril@ro.ru>
2021-04-01 19:59:41 -0400

    Dev: update temporary DataFrame HTML templates

--- genefab3/api/renderers/BrowserDataFrameRenderers.py
+++ genefab3/api/renderers/BrowserDataFrameRenderers.py
@@ -1,9 +1,14 @@
 from flask import Response
 
 
+TABLE_CSS = """
+    table {table-layout: fixed; white-space: nowrap;}
+    th, td {text-align: left;}
+"""
+
+
 def twolevel(obj, indent=None):
     """Placeholder method""" # TODO
-    TABLE_CSS = "table {table-layout: fixed; white-space: nowrap}"
     return Response(
         f"<style>{TABLE_CSS}</style>" +
         obj.fillna("").to_html(index=False, col_space="1in"),
@@ -13,4 +18,8 @@ def twolevel(obj, indent=None):
 
 def threelevel(obj, indent=None):
     """Placeholder method""" # TODO
-    return None
+    return Response(
+        f"<style>{TABLE_CSS}</style>" +
+        obj.fillna("").to_html(index=False, col_space="1in"),
+        mimetype="text/html",
+    )
