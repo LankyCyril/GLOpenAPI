d1adc57a28ff9bfc2afeaf1a27baa758a1f2e6ff
LankyCyril <cyril@ro.ru>
2021-04-04 21:31:45 -0400

    Remove some more legacy code (already unused)

--- app_legacy.py
+++ app_legacy.py
@@ -11,33 +11,8 @@ def root(**kwargs):
     from genefab3.frontend.renderers.docs import interactive_doc
     return interactive_doc(mongo_db, url_root=request.url_root.rstrip("/"))
 
-@app.route("/assays/", methods=["GET"])
-def assays(**kwargs):
-    from genefab3.frontend.getters.metadata import get_assays as getter
-    return render(mongo_db, getter, kwargs, request)
-
-@app.route("/samples/", methods=["GET"])
-def samples(**kwargs):
-    from genefab3.frontend.getters.metadata import get_samples as getter
-    return render(mongo_db, getter, kwargs, request)
-
-@app.route("/files/", methods=["GET"])
-def files(**kwargs):
-    from genefab3.frontend.getters.metadata import get_files as getter
-    return render(mongo_db, getter, kwargs, request)
-
-@app.route("/file/", methods=["GET"])
-def file(**kwargs):
-    from genefab3.frontend.getters.file import get_file as getter
-    return render(mongo_db, getter, kwargs, request)
-
 @app.route("/data/", methods=["GET"])
 def data(**kwargs):
     from genefab3.frontend.getters.data import get_data as getter
     dbs = namedtuple("dbs", "mongo_db, sqlite_db")(mongo_db, SQLITE_DB)
     return render(dbs, getter, kwargs, request)
-
-@app.route("/status/", methods=["GET"])
-def status(**kwargs):
-    from genefab3.frontend.getters.status import get_status as getter
-    return render(mongo_db, getter, kwargs, request)
