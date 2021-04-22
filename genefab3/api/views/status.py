d107fea7a12d1351512104ae408834d66d70e74e
LankyCyril <cyril@ro.ru>
2021-04-21 20:19:11 -0400

    Use blackjack_normalize in views.status

--- genefab3/api/views/status.py
+++ genefab3/api/views/status.py
@@ -1,6 +1,6 @@
-from genefab3.common.utils import iterate_terminal_leaves
+from genefab3.common.utils import iterate_terminal_leaves, blackjack_normalize
 from genefab3.common.exceptions import GeneFabParserException
-from pandas import json_normalize, MultiIndex
+from pandas import MultiIndex
 from datetime import datetime
 from numpy import nan
 from itertools import chain, cycle
@@ -20,7 +20,7 @@ def get(mongo_collections, context):
         status_json = mongo_collections.status.find(
             {}, {"_id": False, **{c: True for c in STATUS_COLUMNS}},
         )
-    status_df = json_normalize(list(status_json), max_level=0).sort_values(
+    status_df = blackjack_normalize(list(status_json), max_depth=1).sort_values(
         by=["report timestamp", "report type"], ascending=[False, True],
     )
     status_df = status_df[[c for c in STATUS_COLUMNS if c in status_df]]
