a205cbd07237c104969a552bc4ae9a10a70f480c
LankyCyril <cyril@ro.ru>
2021-05-06 22:12:09 -0400

    SQLite: WAL mode without autocheckpointing

--- genefab3/db/sql/utils.py
+++ genefab3/db/sql/utils.py
@@ -5,6 +5,20 @@ from sqlite3 import connect, OperationalError
 from genefab3.common.logger import GeneFabLogger
 
 
+def apply_pragma(execute, pragma, value, filename, access_warning):
+    """Apply PRAGMA, test result, warn if unable to apply"""
+    try:
+        execute(f"PRAGMA {pragma} = {value}")
+        status = (execute(f"PRAGMA {pragma}").fetchone() or [None])[0]
+    except (OSError, FileNotFoundError, OperationalError) as e:
+        if access_warning:
+            GeneFabLogger().warning(f"{access_warning}: {e!r}")
+    else:
+        if str(status) != str(value):
+            msg = f"Could not set {pragma} = {value} for database"
+            GeneFabLogger().warning(f"{msg} {filename!r}")
+
+
 @contextmanager
 def sql_connection(filename, desc=None, *, none_ok=False, timeout=600):
     """Preconfigure `filename` if new, allow long timeout (for tasks sent to background), expose connection and execute()"""
@@ -20,25 +34,18 @@ def sql_connection(filename, desc=None, *, none_ok=False, timeout=600):
             access_warning = None
         try:
             with closing(connect(filename, timeout=timeout)) as connection:
-                cursor, pragma = connection.cursor(), "PRAGMA auto_vacuum"
+                execute = connection.cursor().execute
+                args = filename, access_warning
+                apply_pragma(execute, "auto_vacuum", "1", *args)
+                apply_pragma(execute, "journal_mode", "wal", *args)
+                apply_pragma(execute, "wal_checkpoint", "0", *args)
                 try:
-                    cursor.execute(f"{pragma} = 1")
-                    _avac = (cursor.execute(pragma).fetchone() or [0])[0]
-                except (OSError, FileNotFoundError, OperationalError) as e:
-                    if access_warning:
-                        GeneFabLogger().warning(f"{access_warning}: {e!r}")
+                    yield connection, execute
+                except:
+                    connection.rollback()
+                    raise
                 else:
-                    if int(_avac) != 1:
-                        msg = f"Could not set auto_vacuum = 1 for database"
-                        GeneFabLogger().warning(f"{msg} {filename!r}")
-                finally:
-                    try:
-                        yield connection, connection.cursor().execute
-                    except:
-                        connection.rollback()
-                        raise
-                    else:
-                        connection.commit()
+                    connection.commit()
         except (OSError, FileNotFoundError, OperationalError) as e:
             msg = f"Could not connect to SQLite database {filename!r}"
             raise GeneFabConfigurationException(msg, debug_info=repr(e))
