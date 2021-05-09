22398f567039f1f0e11c250138001ed35eb930c9
LankyCyril <cyril@ro.ru>
2021-05-09 02:46:10 -0400

    Dev: boilerplate for a reasonable implementation of SQLiteBlob

--- genefab3/db/sql/core.py
+++ genefab3/db/sql/core.py
@@ -1,16 +1,20 @@
 from itertools import count
-from sqlite3 import OperationalError
+from sqlite3 import OperationalError, Binary
 from genefab3.db.sql.pandas import SQLiteIndexName
 from genefab3.db.sql.utils import sql_connection
 from genefab3.common.utils import validate_no_backtick, validate_no_doublequote
 from genefab3.common.logger import GeneFabLogger
 from threading import Thread
+from genefab3.common.utils import as_is
+from genefab3.common.exceptions import GeneFabConfigurationException
+from datetime import datetime
+from genefab3.common.exceptions import GeneFabDatabaseException
 
 
 class SQLiteObject():
     """Universal wrapper for cached objects"""
  
-    def __init__(self, sqlite_db, table_schemas=None):
+    def __init__(self, *, sqlite_db, table_schemas=None):
         """Initialize SQLiteObject, ensure tables in `sqlite_db`"""
         self.sqlite_db, self.table_schemas = sqlite_db, table_schemas
         self.changed = None
@@ -54,22 +58,22 @@ class SQLiteObject():
     @property
     def trigger(self):
         """Evaluates to True if underlying data in need of update, otherwise False"""
-        msg = "did not provide self.trigger, will never update"
+        msg = "did not define self.trigger, will never update"
         GeneFabLogger().warning(f"{type(self).__name__} {msg}")
         return False
  
     def update(self):
         """Update underlying data in SQLite"""
-        msg = "did not provide self.update(), will never update"
+        msg = "did not define self.update(), will never update"
         GeneFabLogger().warning(f"{type(self).__name__} {msg}")
  
     def retrieve(self):
         """Retrieve underlying data from SQLite"""
-        msg = "did not provide self.retrieve(), will always retrieve `None`"
+        msg = "did not define self.retrieve(), will always retrieve `None`"
         GeneFabLogger().warning(f"{type(self).__name__} {msg}")
         return None
  
-    def _deferrable_cleanup(self):
+    def cleanup(self):
         """Actions to be performed after request completion"""
         pass
  
@@ -81,5 +85,78 @@ class SQLiteObject():
             self.changed = True
         else:
             self.changed = False
-        Thread(target=self._deferrable_cleanup).start()
+        Thread(target=self.cleanup).start()
         return self.retrieve()
+
+
+class SQLiteBlob(SQLiteObject):
+    """Represents an SQLiteObject initialized with a spec suitable for a binary blob"""
+ 
+    def __init__(self, *, sqlite_db, identifier, table, timestamp, data_getter, compressor=as_is, decompressor=as_is, maxdbsize=None):
+        if not table.startswith("BLOBS:"):
+            msg = "Table name for SQLiteBlob must start with 'BLOBS:'"
+            raise GeneFabConfigurationException(msg, table=table)
+        elif maxdbsize is not None:
+            raise NotImplementedError("SQLiteBlob() with set `maxdbsize`")
+        else:
+            SQLiteObject.__init__(
+                self, sqlite_db=sqlite_db, table_schemas={
+                    table: {
+                        "identifier": "TEXT", "blob": "BLOB",
+                        "timestamp": "INTEGER", "retrieved_at": "INTEGER",
+                    },
+                },
+            )
+            self.identifier = validate_no_doublequote(identifier, "identifier")
+            self.table, self.timestamp = table, timestamp
+            self.data_getter = data_getter
+            self.compressor, self.decompressor = compressor, decompressor
+ 
+    def drop(self, execute):
+        execute(f"""DELETE FROM `{self.table}`
+            WHERE `identifier` == "{self.identifier}" """)
+ 
+    @property
+    def trigger(self):
+        """Evaluates to True if underlying data in need of update, otherwise False"""
+        query = f"""SELECT `timestamp` FROM `{self.table}`
+            WHERE `identifier` == "{self.identifier}" """
+        with sql_connection(self.sqlite_db, "blobs") as (_, execute):
+            ret = execute(query).fetchall()
+            if len(ret) == 0:
+                return True
+            elif (len(ret) == 1) and (len(ret[0]) == 1):
+                return (ret[0][0] < self.timestamp)
+            else:
+                msg = "Conflicting trigger values for SQLiteObject"
+                GeneFabLogger().warning(f"{msg}\n  ({self.identifier})")
+                self.drop()
+                return True
+ 
+    def update(self):
+        """Run `self.data_getter` and insert result into `self.table` as BLOB"""
+        blob = Binary(bytes(self.compressor(self.data_getter())))
+        with sql_connection(self.sqlite_db, "blobs") as (_, execute):
+            self.drop(execute)
+            execute(f"""INSERT INTO `{self.table}`
+                (`identifier`,`blob`,`timestamp`,`retrieved_at`)
+                VALUES(?,?,?,?)""", [
+                self.identifier, blob,
+                self.timestamp, int(datetime.now().timestamp()),
+            ])
+ 
+    def retrieve(self):
+        """Take `blob` from `self.table` and decompress with `self.decompressor`"""
+        with sql_connection(self.sqlite_db, "blobs") as (_, execute):
+            query = f"""SELECT `blob` from `{self.table}`
+                WHERE `identifier` == "{self.identifier}" """
+            ret = execute(query).fetchall()
+            if len(ret) == 0:
+                msg = "No data found"
+                raise GeneFabDatabaseException(msg, identifier=self.identifier)
+            elif (len(ret) == 1) and (len(ret[0]) == 1):
+                return self.decompressor(ret[0][0])
+            else:
+                self.drop(execute)
+                msg = "Entries conflict (will attempt to fix on next request)"
+                raise GeneFabDatabaseException(msg, identifier=self.identifier)
