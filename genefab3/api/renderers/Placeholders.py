1a7a41baa911cc6621d0a05542ce5c91037b62e0
LankyCyril <cyril@ro.ru>
2021-05-09 16:27:46 -0400

    EmptyStreamedAnnotationTable: define attributes correctly

--- genefab3/api/renderers/Placeholders.py
+++ genefab3/api/renderers/Placeholders.py
@@ -3,9 +3,7 @@ from genefab3.common.types import StreamedAnnotationTable, NaN
 
 class EmptyStreamedAnnotationTable(StreamedAnnotationTable):
     """Return an empty StreamedAnnotationTable-like"""
-    def __init__(self):
-        self.shape, self.n_index_levels = (0, 0), 1
-        self.index_levels, self.column_levels = ["*"], ["*", "*"]
-        self.index_names, self.columns = [["*"]], [["*", "*"]]
-        self.index, self.values = [[NaN]], [[NaN]]
-        self.move_index_boundary = lambda *a, **k: None
+    __init__ = move_index_boundary = lambda *a, **k: None
+    shape, n_index_levels = (0, 0), 1
+    index_levels, column_levels = ["*"], ["*", "*"]
+    index = values = [[NaN]]
