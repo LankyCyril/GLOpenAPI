eaa47a1fc7a681ae8cfa233704c95f5730acee30
LankyCyril <cyril@ro.ru>
2021-07-07 22:06:50 -0400

    Dev: 2PL implementation via fd counts

--- genefab3/db/sql/utils.py
+++ genefab3/db/sql/utils.py
@@ -1,6 +1,6 @@
+from genefab3.common.exceptions import GeneFabLogger, GeneFabDatabaseException
 from genefab3.common.exceptions import GeneFabConfigurationException
 from os import path, access, W_OK, stat, remove, makedirs
-from genefab3.common.exceptions import GeneFabLogger, GeneFabDatabaseException
 from datetime import datetime
 from filelock import Timeout as FileLockTimeoutError, FileLock
 from glob import iglob
@@ -9,21 +9,35 @@ from contextlib import contextmanager, closing
 from genefab3.common.utils import timestamp36
 from sqlite3 import connect, OperationalError
 from threading import Thread
+from subprocess import call
+from time import sleep
 
 
-def check_database_validity(filename, desc):
-    """Make sure database exists and can be written to"""
-    if filename is None:
-        msg = f"SQLite database ({desc!r}) was not specified"
-        raise GeneFabConfigurationException(msg)
+_logd = GeneFabLogger.debug
+_logw = GeneFabLogger.warning
+_loge = GeneFabLogger.error
+
+
+def apply_pragma(execute, pragma, value, sqlite_db):
+    """Apply PRAGMA, test result, warn if unable to apply"""
+    try:
+        execute(f"PRAGMA {pragma} = {value}")
+        status = (execute(f"PRAGMA {pragma}").fetchone() or [None])[0]
+    except (OSError, FileNotFoundError, OperationalError) as e:
+        msg = f"Database {sqlite_db!r} may not be writable"
+        _logw(msg, exc_info=e)
     else:
-        access_warning = f"SQLite file {filename!r} may not be writable"
-        if path.exists(filename) and (not access(filename, W_OK)):
-            GeneFabLogger.warning(f"{access_warning}: path.access()")
-            potential_access_warning = None
-        else:
-            potential_access_warning = access_warning
-        return potential_access_warning
+        if str(status) != str(value):
+            msg = f"Could not set {pragma} = {value} for database {sqlite_db!r}"
+            _logw(msg)
+
+
+def apply_all_pragmas(sqlite_db, execute, timeout):
+    """Apply all relevant PRAGMAs at once: auto_vacuum, WAL, wal_autocheckpoint, busy_timeout"""
+    apply_pragma(execute, "auto_vacuum", "1", sqlite_db)
+    apply_pragma(execute, "journal_mode", "wal", sqlite_db)
+    apply_pragma(execute, "wal_autocheckpoint", "0", sqlite_db)
+    apply_pragma(execute, "busy_timeout", str(int(timeout*1000)), sqlite_db)
 
 
 def clear_lock_if_stale(lockfilename, max_filelock_age_seconds=7200, raise_errors=True):
@@ -37,16 +51,16 @@ def clear_lock_if_stale(lockfilename, max_filelock_age_seconds=7200, raise_error
         if raise_errors:
             raise GeneFabConfigurationException(msg, debug_info=repr(e))
         else:
-            GeneFabLogger.error(msg, exc_info=e)
+            _loge(msg, exc_info=e)
             return
     else:
         if not access(lockfilename, W_OK):
-            GeneFabLogger.warning(f"{lockfilename} may not be writable")
+            _logw(f"{lockfilename} may not be writable")
     lock_age_seconds = (datetime.now() - lockfile_ctime).total_seconds()
     if lock_age_seconds > max_filelock_age_seconds:
         try:
             msg = f"{lockfilename} ({lock_age_seconds} seconds old)"
-            GeneFabLogger.debug(f"Clearing stale lock:\n  {msg}")
+            _logd(f"Clearing stale lock:\n  {msg}")
             try: # intercept if possible, prevent other instances stealing lock
                 with FileLock(lockfilename, timeout=1e-10):
                     remove(lockfilename)
@@ -59,97 +73,39 @@ def clear_lock_if_stale(lockfilename, max_filelock_age_seconds=7200, raise_error
             if raise_errors:
                 raise GeneFabConfigurationException(msg, debug_info=repr(e))
             else:
-                GeneFabLogger.error(msg, exc_info=e)
+                _loge(msg, exc_info=e)
 
 
-def clear_stale_locks(filename, raise_errors=False):
-    """Clear abandoned (junked) lockfiles that are in the same directory as `filename`"""
-    directory, _ = path.split(filename)
-    for lockfilename in iglob(f"{directory}/.*.lock"):
-        clear_lock_if_stale(lockfilename, raise_errors=raise_errors)
-
-
-def get_filelock(filename, identifier, locking_tier):
-    """If `locking_tier`, stage lockfile and activate DEBUG-level logger"""
-    if locking_tier:
-        directory, name = path.split(filename)
-        if identifier is None:
-            lockfilename = path.join(directory, f".{name}.lock")
-        else:
-            id_hash = md5(identifier.encode()).hexdigest()
-            lockfilename = path.join(directory, f".{name}.{id_hash}.lock")
-        clear_lock_if_stale(lockfilename, raise_errors=True)
-        lock = FileLock(lockfilename)
-        log_if_lock = GeneFabLogger.debug
-    else:
-        @contextmanager
-        def nullcontext():
-            yield
-        lock = nullcontext()
-        log_if_lock = lambda *a, **k: None
-    return lock, log_if_lock
-
-
-def apply_pragma(execute, pragma, value, filename, potential_access_warning):
-    """Apply PRAGMA, test result, warn if unable to apply"""
-    try:
-        execute(f"PRAGMA {pragma} = {value}")
-        status = (execute(f"PRAGMA {pragma}").fetchone() or [None])[0]
-    except (OSError, FileNotFoundError, OperationalError) as e:
-        if potential_access_warning:
-            GeneFabLogger.warning(f"{potential_access_warning}", exc_info=e)
-    else:
-        if str(status) != str(value):
-            msg = f"Could not set {pragma} = {value} for database"
-            GeneFabLogger.warning(f"{msg} {filename!r}")
-
-
-def apply_all_pragmas(filename, execute, timeout, potential_access_warning=None):
-    """Apply all relevant PRAGMAs at once: auto_vacuum, WAL, wal_autocheckpoint, busy_timeout"""
-    args = filename, potential_access_warning
-    apply_pragma(execute, "auto_vacuum", "1", *args)
-    apply_pragma(execute, "journal_mode", "wal", *args)
-    apply_pragma(execute, "wal_autocheckpoint", "0", *args)
-    apply_pragma(execute, "busy_timeout", str(int(timeout*1000)), *args)
-
-
-@contextmanager
-def SQLTransaction(filename, desc=None, *, identifier=None, locking_tier=False, timeout=600):
-    """Preconfigure `filename` if new, allow long timeout (for tasks sent to background), expose connection and execute()"""
-    desc, _tid = desc or filename, timestamp36()
-    potential_access_warning = check_database_validity(filename, desc)
-    lock, log_if_lock = get_filelock(filename, identifier, locking_tier)
-    log_if_lock(f"{desc} @ {_tid}: acquiring lock...")
-    with lock:
-        log_if_lock(f"{desc} @ {_tid}: acquired lock!")
-        try:
-            _kw = dict(timeout=timeout, isolation_level=None)
-            with closing(connect(filename, **_kw)) as connection:
-                log_if_lock(f"{desc} @ {_tid}: begin transaction")
-                execute = connection.execute
-                apply_all_pragmas(
-                    filename, execute, timeout, potential_access_warning,
-                )
-                execute("BEGIN")
-                try:
-                    yield connection, execute
-                except Exception as e:
-                    connection.rollback()
-                    msg = "rolling back transaction due to"
-                    GeneFabLogger.warning(f"{desc} @ {_tid}: {msg} {e!r}")
-                    raise
+def fds_exceed(filename, maxcount):
+    """Check if number of open file descriptors for `filename` exceeds `maxcount`"""
+    from sys import platform
+    from tempfile import NamedTemporaryFile
+    global fds_exceed # this function bootstraps itself on first call
+    if not getattr(fds_exceed, "bootstrapped", None):
+        if platform.startswith("linux") and path.isdir("/proc"):
+            def _fds_exceed(filename, maxcount):
+                realpath, n = path.realpath(filename), 0
+                for fd in iglob("/proc/[0-9]*/fd/*"):
+                    if path.realpath(fd) == realpath:
+                        n += 1
+                        if n > maxcount:
+                            return True
                 else:
-                    msg = f"{desc} @ {_tid}: committing transaction"
-                    GeneFabLogger.debug(msg)
-                    connection.commit()
-        except (OSError, FileNotFoundError, OperationalError) as e:
-            msg = "Data could not be retrieved"
-            raise GeneFabDatabaseException(msg, debug_info=repr(e))
-        finally:
-            log_if_lock(f"{desc} @ {_tid}: released lock")
-            connection.close()
-            if locking_tier:
-                Thread(target=lambda: clear_stale_locks(filename)).start()
+                    return False
+            with NamedTemporaryFile(mode="w") as tf:
+                with open(tf.name):
+                    if (not _fds_exceed(tf.name, 1)) or _fds_exceed(tf.name, 2):
+                        problem = "test poll of /proc returned unexpected value"
+                        msg = f"Cannot set up ReadLock methods: {problem}"
+                        raise GeneFabConfigurationException(msg)
+                    else:
+                        fds_exceed = _fds_exceed
+                        fds_exceed.bootstrapped = True
+                        return _fds_exceed(filename, maxcount)
+        else:
+            problem = "/proc not available and/or not a Linux/POSIX system"
+            msg = f"Cannot set up read lock methods: {problem}"
+            raise GeneFabConfigurationException(msg)
 
 
 class SQLT():
@@ -169,42 +125,19 @@ class SQLT():
             self.cwd, self.identifier = cwd, identifier
             _, name = path.split(sqlite_db)
             if identifier is None:
-                self.prefix = path.join(cwd, name)
+                self._lockfilename = path.join(cwd, f"{name}.lock")
             else:
                 id_hash = md5(identifier.encode()).hexdigest()
-                self.prefix = path.join(cwd, f"{name}.{id_hash}")
+                self._lockfilename = path.join(cwd, f"{name}.{id_hash}.lock")
+            clear_lock_if_stale(self._lockfilename, raise_errors=True)
  
     @contextmanager
-    def lock(self, postfix, *, exclusive):
-        lockfilename = f"{self.prefix}.{postfix}.lock"
-        clear_lock_if_stale(
-            lockfilename, raise_errors=True,
-            max_filelock_age_seconds=self.max_filelock_age_seconds,
-        )
-        if exclusive:
-            with FileLock(lockfilename) as lock:
-                yield lock
-        else:
-            watch = True
-            def _keep_acquiring(timeout=.05):
-                while watch:
-                    try:
-                        with FileLock(lockfilename, timeout=timeout):
-                            pass
-                    except FileLockTimeoutError:
-                        pass
-            watcher = Thread(target=_keep_acquiring)
-            watcher.start()
-            yield
-            watch = False
- 
-    @contextmanager
-    def _connect(self, desc=None):
-        desc, _tid = desc or self.sqlite_db, timestamp36()
+    def _connect(self, desc, _tid):
+        prelude = f"SQLT._connect ({desc} @ {_tid})"
         try:
             _kw = dict(timeout=self.timeout, isolation_level=None)
             with closing(connect(self.sqlite_db, **_kw)) as connection:
-                GeneFabLogger.debug(f"{desc} @ {_tid}: begin transaction")
+                _logd(f"{prelude}: begin transaction")
                 execute = connection.execute
                 apply_all_pragmas(self.sqlite_db, execute, self.timeout)
                 execute("BEGIN")
@@ -212,39 +145,57 @@ class SQLT():
                     yield connection, execute
                 except Exception as e:
                     connection.rollback()
-                    msg = "rolling back transaction due to"
-                    GeneFabLogger.warning(f"{desc} @ {_tid}: {msg} {e!r}")
+                    msg = f"{prelude}: transaction failed, rolling back"
+                    _logw(msg, exc_info=e)
                     raise
                 else:
-                    msg = f"{desc} @ {_tid}: committing transaction"
-                    GeneFabLogger.debug(msg)
+                    _logd(f"{prelude}: committing transaction")
                     connection.commit()
         except (OSError, FileNotFoundError, OperationalError) as e:
             msg = "Data could not be retrieved"
             raise GeneFabDatabaseException(msg, debug_info=repr(e))
         finally:
-            #log_if_lock(f"{desc} @ {_tid}: released lock")
             connection.close()
-            #if locking_tier:
-            #    Thread(target=lambda: clear_stale_locks(filename)).start()
+            def _clear_stale_locks():
+                for lockfilename in iglob(f"{self.cwd}/*.lock"):
+                    clear_lock_if_stale(lockfilename, raise_errors=False)
+            Thread(target=_clear_stale_locks).start()
  
     @contextmanager
     def readable(self, desc=None):
-        read_lock = self.lock("r", exclusive=False)
-        write_lock = self.lock("w", exclusive=True, release_immediately=True)
-        with read_lock: # prevents NEW self.writable from locking on
-            with write_lock: # waits for EXISTING self.writable to finish
-                with self.connect(desc) as (connection, execute):
+        """2PL: lock that is non-exclusive w.r.t. other `SQLT.readable`s, but exclusive w.r.t. `SQLT.writable`"""
+        desc, _tid = desc or self.sqlite_db, timestamp36()
+        prelude = f"SQLT.readable ({desc} @ {_tid})"
+        _logd(f"{prelude}: waiting for write locks to release...")
+        with FileLock(self._lockfilename) as lock:
+            # grow handle count; prevents new write locks:
+            _logd(f"{prelude}: increasing read lock count (++)")
+            with open(self._lockfilename):
+                # release hard lock immediately; new `WriteLock`s will not be
+                # able to lock on, because read handle count is above zero:
+                lock.release()
+                # finally, initiate SQL connection:
+                with self._connect(desc, _tid) as (connection, execute):
                     yield connection, execute
+                _logd(f"{prelude}: decreasing read lock count (--)")
  
     @contextmanager
-    def writable(self, desc=None):
-        read_lock = self.lock("r", exclusive=True)
-        write_lock = self.lock("w", exclusive=True)
-        with read_lock: # waits for EXISTING self.readable to finish
-            with write_lock: # prevents all other operations from locking on
-                with self.connect(desc) as (connection, execute):
-                    yield connection, execute
+    def writable(self, desc=None, poll_interval=.1):
+        """2PL: lock that is exclusive w.r.t. both `SQLT.readable`s and `SQLT.writable`s"""
+        desc, _tid = desc or self.sqlite_db, timestamp36()
+        prelude = f"SQLT.writable ({desc} @ {_tid})"
+        _logd(f"{prelude}: obtaining write lock...")
+        # obtain hard lock; prevents all other locks:
+        with FileLock(self._lockfilename):
+            # wait for old `ReadLock`s to finish:
+            _logd(f"{prelude}: write lock obtained!")
+            _logd(f"{prelude}: waiting for all read locks to release...")
+            while fds_exceed(self._lockfilename, 1):
+                sleep(poll_interval)
+            # finally, initiate SQL connection:
+            with self._connect(desc) as (connection, execute):
+                yield connection, execute
+                _logd(f"{prelude}: releasing write lock")
 
 
 def reraise_operational_error(obj, e):
