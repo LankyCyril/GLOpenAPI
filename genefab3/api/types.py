5809310b997bcb21c7dee0fb60f33df2f0298142
LankyCyril <cyril@ro.ru>
2021-12-14 13:18:36 -0500

    Move Routes() class to genefab3.api.types

---
+++ genefab3/api/types.py
@@ -0,0 +1,29 @@
+from functools import wraps
+
+
+class Routes():
+    """Base class for registered endpoints"""
+ 
+    def __init__(self, genefab3_client):
+        self.genefab3_client = genefab3_client
+ 
+    def register_endpoint(endpoint=None):
+        """Decorator that adds `endpoint` and `fmt` attributes to class method"""
+        def outer(method):
+            @wraps(method)
+            def inner(*args, **kwargs):
+                return method(*args, **kwargs)
+            if endpoint:
+                inner.endpoint = endpoint
+            elif hasattr(method, "__name__"):
+                if isinstance(method.__name__, str):
+                    inner.endpoint = "/" + method.__name__ + "/"
+            return inner
+        return outer
+ 
+    def items(self):
+        """Iterate over methods of `Routes` object that have `endpoint` attribute"""
+        for name in dir(self):
+            method = getattr(self, name)
+            if isinstance(getattr(method, "endpoint", None), str):
+                yield method.endpoint, method
--- genefab3/common/types.py
+++ genefab3/common/types.py
@@ -1,7 +1,6 @@
 from itertools import tee
 from collections.abc import Callable
 from genefab3.common.exceptions import GeneFabConfigurationException
-from functools import wraps
 from flask import Response
 from marshal import dumps as marsh
 from collections import OrderedDict
@@ -68,34 +67,6 @@ class Adapter():
             )
         else:
             return [ns for ns in names if ns == name]
-
-
-class Routes():
-    """Base class for registered endpoints"""
- 
-    def __init__(self, genefab3_client):
-        self.genefab3_client = genefab3_client
- 
-    def register_endpoint(endpoint=None):
-        """Decorator that adds `endpoint` and `fmt` attributes to class method"""
-        def outer(method):
-            @wraps(method)
-            def inner(*args, **kwargs):
-                return method(*args, **kwargs)
-            if endpoint:
-                inner.endpoint = endpoint
-            elif hasattr(method, "__name__"):
-                if isinstance(method.__name__, str):
-                    inner.endpoint = "/" + method.__name__ + "/"
-            return inner
-        return outer
- 
-    def items(self):
-        """Iterate over methods of `Routes` object that have `endpoint` attribute"""
-        for name in dir(self):
-            method = getattr(self, name)
-            if isinstance(getattr(method, "endpoint", None), str):
-                yield method.endpoint, method
 
 
 class ResponseContainer():
